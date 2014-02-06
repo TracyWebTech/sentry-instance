146bc1309bfadc247d86faaf57b09696e93a74a9
Sergio Oliveira <sergio@tracy.com.br>
2014-02-06 11:12:44 -0200

    initial

---
+++ puppet/manifests/init.pp
@@ -0,0 +1,33 @@
+
+include supervisor
+
+supervisor::app { '':
+  command   => '.virtualenvs/sentry/bin/sentry --config=sentry-config.py start',
+  directory => '/home/sentry/',
+  user      => 'sentry',
+}
+
+package { 'python-dev':
+  ensure => installed,
+}
+
+package { 'libev-dev':
+  ensure => installed,
+}
+
+package { 'postgresql-server-dev-9.2':
+  ensure => installed,
+}
+
+package { 'python-pip':
+  ensure => installed,
+}
+
+package { 'postgresql-9.2':
+  ensure => installed,
+}
+
+package { 'build-essential':
+  ensure => installed,
+}
+
