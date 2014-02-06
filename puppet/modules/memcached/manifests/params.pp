975b9e7991de339a023a5562336e1c370ecc3b20
Sergio Oliveira <sergio@tracy.com.br>
2014-02-06 18:01:33 -0200

    Updating puppet-memcached

--- puppet/modules/memcached/manifests/params.pp
+++ puppet/modules/memcached/manifests/params.pp
@@ -1,15 +1,7 @@
+# == Class: memcached::params
+#
 class memcached::params {
-
-  # Sets osfamily_
-  #   osfamily_ is the same than ::osfamily but also considers
-  #   Amazon Linux as member of RedHat family
-  if ($::osfamily == 'Linux' and $::operatingsystem == 'Amazon') {
-    $osfamily_ = 'RedHat'
-  } else {
-    $osfamily_ = $::osfamily
-  }
-
-  case $osfamily_ {
+  case $::osfamily {
     'Debian': {
       $package_name      = 'memcached'
       $service_name      = 'memcached'
@@ -29,7 +21,20 @@ class memcached::params {
       $user              = 'memcached'
     }
     default: {
-      fail("Unsupported platform: ${osfamily_}")
+      case $::operatingsystem {
+        'Amazon': {
+          $package_name      = 'memcached'
+          $service_name      = 'memcached'
+          $service_hasstatus = true
+          $dev_package_name  = 'libmemcached-devel'
+          $config_file       = '/etc/sysconfig/memcached'
+          $config_tmpl       = "${module_name}/memcached_sysconfig.erb"
+          $user              = 'memcached'
+        }
+        default: {
+          fail("Unsupported platform: ${::osfamily}/${::operatingsystem}")
+        }
+      }
     }
   }
 }
